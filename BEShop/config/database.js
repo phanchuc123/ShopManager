724a465cc8954df66295c0d8d0fb50d8e477d075
phanchuc123 <anhaikhoilop520182019@gmail.com>
2026-06-23 14:57:06 +0700

    FIx:enable  dynamic SSL for remote db

--- BEShop/config/database.js
+++ BEShop/config/database.js
@@ -9,7 +9,8 @@ const connection = mysql.createPool({
     port: process.env.DB_PORT || '3306',
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: process.env.DB_HOST && process.env.DB_HOST !== 'localhost' ? { rejectUnauthorized: false } : undefined
 });
 
 module.exports = connection;
